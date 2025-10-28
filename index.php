acbbcfa7cb6a3b0a0cde00ae241cc336a2011b25
Gabrielovato <gabilovato45@gmail.com>
2025-10-28 00:29:26 -0300

    Agregue estructura carrusel

--- index.php
+++ index.php
@@ -23,27 +23,32 @@ $resultado_locales = mysqli_query($conexion, $sql_locales);
 
     <!-- CARRUSEL -->
 
-    <section class="carrusel">
-        <div class="carrusel">
-            <div class="container">
-                <div class="container-fluid p-0">
-                <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="assets/img/40578.png" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item active">
-                            <img src="assets/img/placeholder2.png" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item active">
-                            <img src="assets/img/placeholder3.png" class="d-block w-100" alt="...">
-                        </div>
-                    </div>
-                    </div>
-                </div> 
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="/Descuento-City/assets/img/carrusel/40578.png" class="d-block w-100" alt="Promociones Descuento City" style="height: 400px; object-fit: cover;">
+            </div>
+            <div class="carousel-item">
+                <img src="/Descuento-City/assets/img/carrusel/40578.png" class="d-block w-100" alt="Locales participantes" style="height: 400px; object-fit: cover;">
+            </div>
+            <div class="carousel-item">
+                <img src="/Descuento-City/assets/img/carrusel/40578.png" class="d-block w-100" alt="Promociones vigentes" style="height: 400px; object-fit: cover;">
             </div>
         </div>
-    </section>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Anterior</span>
+            </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Siguiente</span>
+        </button>
+    </div>
 
     <!-- LOCALES -->
 
@@ -97,9 +102,11 @@ $resultado_locales = mysqli_query($conexion, $sql_locales);
 
 
     <!-- FOOTER -->
-     
+
     <?php
     include("includes/footer.php");
     ?>  
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
