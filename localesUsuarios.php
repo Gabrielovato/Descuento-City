728df0506494800a89296ba1376dd6dd36dae73d
selebrassart <selenebrassart1@gmail.com>
2025-10-28 20:53:38 -0300

    agrego cambios en esta rama

---
+++ localesUsuarios.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Promociones - Invitado</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/estilos.css">
+</head>
+<body>
+<?php include("includes/header.php"); ?>
+
+<img src="/Descuento-City/assets/img/locales-portada.png" class="img-fluid" alt="...">
+
+<div class="container my-4">
+
+
+
+<?php include("includes/footer.php"); ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
