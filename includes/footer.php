233175d013e75a9ecb07716fbccf6a47e274aa49
Valentino <valentino.ittaval@gmail.com>
2025-10-28 21:33:19 -0300

    Crear footer + estilos

--- includes/footer.php
+++ includes/footer.php
@@ -3,12 +3,56 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href=".css">
-    <title>Document</title>
+    <link rel="stylesheet" href="/Descuento-City/assets/css/footer.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <!-- Font Awesome (para íconos de teléfono, ubicación, mail, redes) -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" crossorigin="anonymous" />
+    <title>Descuento City</title>
 </head>
 <body>
-    <footer>
-        Descuento City - Copyright
+    <footer class = "footer">
+        <div class="container">
+            <div class="footer-row">
+                <div class="footer-links">
+                    <h4>Página</h4>
+                    <ul>
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Nuestros locales</a></li>
+                        <li><a href="#">Promociones</a></li>
+                        <li><a href="#">Novedades</a></li>
+                        <li><a href="#">Registrarse</a></li>
+                    </ul>
+                </div>
+
+                <div class="footer-links">
+                    <h4>Sobre nosotros</h4>
+                    <ul>
+                        <li><a href="tel:+543413325148"><i class="fa-solid fa-phone"></i> (+54) 341-332-5148</a></li>
+                        <li><a href="https://maps.google.com/?q=Jun%C3%ADn+501" target="_blank" rel="noopener"><i class="fa-solid fa-location-dot"></i> Junín 501</a></li>
+                        <li><a href="mailto:info.shop@gmail.com"><i class="fa-solid fa-envelope"></i> info.shop@gmail.com</a></li>
+                    </ul>
+                </div>
+
+                <div class="social-link">
+                    <h4>Seguinos en</h4>
+                    <ul>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                    </ul>
+                </div>
+                <div class="footer-links">
+                    <h4>Ubicación</h4>
+                    <div class="map-responsive">
+                        <!-- Cambia la query (q=...) por la dirección o coordenadas que necesites -->
+                        <iframe
+                            src="https://maps.google.com/maps?q=Jun%C3%ADn%20501&z=15&output=embed"
+                            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
     </footer>
 </body>
 </html>
